0b4a6833c8f99e1c97fafb98c26f0c4530821c4e
danu44-1 <danudimas042@gmail.com>
2026-05-12 14:23:32 +0700

    Update pengerjaan pretest

--- praktikum_pbo-Mod8-pretest/src/Main.java
+++ praktikum_pbo-Mod8-pretest/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Karyawan karyawan = new Programmer(50.0,"Danu");
 
-        System.out.println("Nama : " + karyawan.nama + "Gaji : " + karyawan.);
+        System.out.println("Nama : " + karyawan.nama + "Gaji : " + karyawan.gajiPokok);
     }
 
 }
